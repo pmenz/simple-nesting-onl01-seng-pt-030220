0dd05d055b448cb1d829f7eb39892e7b978fc286
Pablo Menz <p.menz.r@gmail.com>
2020-03-15 22:53:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,7 +119,7 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C", "Assembly"]
+          :languages => ["C"]
         }
      }
 
