8ab4c00fef60647ff25be5f58c4ee91900ba1f06
Pablo Menz <p.menz.r@gmail.com>
2020-03-15 22:46:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -78,7 +79,8 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+        :
      }
 
     
